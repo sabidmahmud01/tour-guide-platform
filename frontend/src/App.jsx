3c0ac950bcd969be10626da6f9e6a6cad9559d85
SabidMahmud <sabidmahmud01@gmail.com>
2025-12-27 23:43:53 -0600

    Initial commit: React frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Local Guide Platform</h1>
+      <p>Find trusted local guides anywhere in the world.</p>
+    </div>
+  );
+}
+
+export default App;
